8335e20cd25e84d8018541fc3435fe7398d83187
Joris Falter <joris@jorisfalter.com>
2023-03-29 12:14:02 +0700

    x

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const { createCanvas } = require("canvas");
-const fs = require("fs");
+const Jimp = require("jimp");
 
-// Dimensions for the image
-const width = 1200;
-const height = 627;
+const quote = "Your very long quote goes here...";
+const imageWidth = 1080;
+const imageHeight = 1080;
+const textHeight = 200; // fixed height for the text
+const textMaxWidth = 900;
 
-// Instantiate the canvas object
-const canvas = createCanvas(width, height);
-const context = canvas.getContext("2d");
-
-// Fill the rectangle with purple
-context.fillStyle = "#764abc";
-context.fillRect(0, 0, width, height);
-
-// Write the image to file
-const buffer = canvas.toBuffer("image/png");
-fs.writeFileSync("./image.png", buffer);
+new Jimp(imageWidth, imageHeight, "#FF7F50", (err, image) => {
+  // salmon orange color
+  if (err) {
+    console.log(err);
+  } else {
+    Jimp.loadFont(Jimp.FONT_SANS_64_WHITE)
+      .then((font) => {
+        const textX = imageWidth / 2 - Jimp.measureText(font, quote) / 2; // calculate the X position for the quote text
+        const textY = imageHeight / 2 - textHeight / 2; // calculate the Y position for the quote text
+        image.print(font, textX, textY, quote, textMaxWidth); // add the text to the image using the loaded font
+        image.write("quote.png");
+        console.log("Image created!");
+      })
+      .catch((err) => console.log(err));
+  }
+});
